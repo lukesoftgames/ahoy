120e81abc14c59d63d2330e8b7ae4dd7c0788d0f
Benji <s1709221@ed.ac.uk>
2019-04-10 23:07:34 +0100

    better camera following

--- game_files/ahoy/Assets/Scripts/SimpleFollowPlayer.cs
+++ game_files/ahoy/Assets/Scripts/SimpleFollowPlayer.cs
@@ -7,12 +7,21 @@ public class SimpleFollowPlayer : MonoBehaviour
     // Start is called before the first frame update
     public Transform player;
     public Vector3 offset;
+    public float cameraSpeedScaler;
+    private Vector3 currentCameraPosition;
+    private Vector3 distError;
 
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = offset + new Vector3(player.position.x, player.position.y, 0);
+        Vector3 desiredPosition = player.position + offset;
+        currentCameraPosition = transform.position - offset;
+        distError = player.position - currentCameraPosition;
+        Debug.Log(transform.forward*100);
+        float smoothSpeed = Mathf.Pow(distError.magnitude,2)/100 * Time.deltaTime * cameraSpeedScaler;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
         
     }
 }
