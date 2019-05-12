52a429687a92cd7be4c0a619fa0257febdd7e1e8
Frederick Bawden <frederickbawden@c114117.wlan.net.ed.ac.uk>
2019-05-12 17:47:36 +0100

    Linear Movement No Rotation

---
+++ game_files/ahoy/Assets/Scripts/Ship/RotateShipModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateShipModel : MonoBehaviour
+{
+    public float rotationMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void RotateModel(Vector3 rotate) {
+        Debug.Log(rotate);
+       
+        transform.Rotate(rotate);
+        
+           
+    
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ game_files/ahoy/Assets/Scripts/Ship/ShipMovementController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ShipMovementController : MonoBehaviour
+{
+   
+    public RotateShipModel shipModel;
+    private float bearing;
+    [Header("Game-Move Parameters")]
+    public float maxSpeed;
+    private float currentSteeringForce = 0;
+    public float maxSteeringForce;
+    
+    public float[] speed;
+    public float[] turning; 
+
+    public int currentSpeed;
+    
+    /*
+        Turn the ship to the target
+    */
+    public void Turn(float dir) 
+    {  
+        Debug.Log(currentSteeringForce);
+        if (dir != 0) {
+            if (Mathf.Abs(currentSteeringForce) <  turning[currentSpeed]) {
+                currentSteeringForce += dir*Time.deltaTime;
+            }
+        } else {
+                if (Mathf.Abs(currentSteeringForce) < 0.01) {
+                    currentSteeringForce = 0;
+                } else {
+                    if (currentSteeringForce < 0) {
+                         currentSteeringForce += Time.deltaTime/10;
+                    } else {
+                         currentSteeringForce -= Time.deltaTime/10;
+                    }
+                }
+        }
+        Vector3 rotationVector = new Vector3(0, currentSteeringForce, 0);
+        transform.Rotate(rotationVector);
+        shipModel.RotateModel(new Vector3(currentSteeringForce*-1,0,0));
+        Step();
+    } 
+    void Step() {
+        transform.position = transform.position + transform.right * Time.deltaTime * speed[currentSpeed];
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    // Update is called once per frame
+    void Update()
+    {  
+           
+    }
+}
